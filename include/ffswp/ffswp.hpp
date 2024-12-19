b21df5ac98163e3c2441061e4f410dbd99283a84
otenim <snake_istobelieve@keio.jp>
2024-12-19 17:11:45 +0900

    chore: initial

--- include/ffswp/ffswp.hpp
+++ include/ffswp/ffswp.hpp
@@ -22,11 +22,6 @@
 #include <opencv2/opencv.hpp>
 #include <tensorrt_utils/inference_engine.hpp>
 
-// Define tensor names for the FastFaceSwapper
-#define FFSWP_TENSORNAME_CONDITION "condition"
-#define FFSWP_TENSORNAME_MASK "mask"
-#define FFSWP_TENSORNAME_INPAINTED "inpainted"
-
 namespace ffswp {
 
 class FastFaceSwapper {
@@ -39,61 +34,66 @@ class FastFaceSwapper {
 
   // Constructor for FastFaceSwapper class
   // Initializes the inference engine with the provided engine path and logger
-  FastFaceSwapper(const std::filesystem::path& enginePath, nvinfer1::ILogger& logger) {
+  FastFaceSwapper(const std::filesystem::path& enginePath, nvinfer1::ILogger& logger,
+                  const std::vector<std::string> names = {"condition", "mask", "inpainted"}) {
     // Create inference engine
     inferEngine_ = std::make_unique<tensorrt_utils::InferenceEngine>(enginePath, logger);
 
-    // Validate inference engine input tensor names
-    const auto inputNames = inferEngine_->getInputNames();
-    if (std::find(inputNames.begin(), inputNames.end(), FFSWP_TENSORNAME_CONDITION) ==
-            inputNames.end() ||
-        std::find(inputNames.begin(), inputNames.end(), FFSWP_TENSORNAME_MASK) ==
-            inputNames.end()) {
-      throw std::runtime_error(
-          absl::StrFormat("Expected names of input tensors: %s, %s. But missing one or both.",
-                          FFSWP_TENSORNAME_CONDITION, FFSWP_TENSORNAME_MASK));
+    // Validate given names
+    if (names.size() != 3) {
+      throw std::runtime_error(absl::StrFormat(
+          "Expected 3 names for input/output tensors. But got wrong number = %d.", names.size()));
     }
 
-    // Validate inference engine output tensor names
-    const auto outputNames = inferEngine_->getOutputNames();
-    if (std::find(outputNames.begin(), outputNames.end(), FFSWP_TENSORNAME_INPAINTED) ==
-        outputNames.end()) {
-      throw std::runtime_error(absl::StrFormat(
-          "Expected names of outptu tensors: %s. But got wrong name.", FFSWP_TENSORNAME_INPAINTED));
+    // Set io tensor names
+    ioNameCondition_ = names[0];
+    ioNameMask_ = names[1];
+    ioNameOutput_ = names[2];
+
+    // Validate given io names
+    auto inputNames = inferEngine_->getInputNames();
+    auto ioNames = inferEngine_->getOutputNames();
+    ioNames.insert(ioNames.end(), inputNames.begin(), inputNames.end());
+    for (const auto& name : names) {
+      if (std::find(ioNames.begin(), ioNames.end(), name) == ioNames.end()) {
+        throw std::runtime_error(absl::StrFormat(
+            "Could not find any io tensors of name = %s in the tensorRT engine.", name));
+      }
     }
 
     // Get tensor shapes from the inference engine
-    const auto& trtEngine = inferEngine_->getEngine();
-    const auto conditionDims = trtEngine.getTensorShape(FFSWP_TENSORNAME_CONDITION);
-    const auto maskDims = trtEngine.getTensorShape(FFSWP_TENSORNAME_MASK);
-    const auto inpaintedDims = trtEngine.getTensorShape(FFSWP_TENSORNAME_INPAINTED);
-    if (conditionDims.nbDims != 4) {
-      throw std::runtime_error(absl::StrFormat("Invalid number of dimensions for %s tensor: %d.",
-                                               FFSWP_TENSORNAME_CONDITION, conditionDims.nbDims));
+    const auto& engine = inferEngine_->getEngine();
+    const auto dimsCondition = engine.getTensorShape(ioNameCondition_.c_str());
+    const auto dimsMask = engine.getTensorShape(ioNameMask_.c_str());
+    const auto dimsOutput = engine.getTensorShape(ioNameOutput_.c_str());
+    if (dimsCondition.nbDims != 4) {
+      throw std::runtime_error(
+          absl::StrFormat("Invalid number of dimensions for %s tensor: %d (expected 4).",
+                          ioNameCondition_, dimsCondition.nbDims));
     }
-    const auto imgH = conditionDims.d[2];
-    const auto imgW = conditionDims.d[3];
+    const auto imgH = dimsCondition.d[2];
+    const auto imgW = dimsCondition.d[3];
 
     // Check channel sizes of io tensors
-    if (!tensorrt_utils::expectDims(conditionDims, {-1, 3, imgH, imgW})) {
-      throw std::runtime_error(absl::StrFormat("Invalid shape for %s tensor: %s.",
-                                               FFSWP_TENSORNAME_CONDITION,
-                                               tensorrt_utils::dimsToStr(conditionDims)));
+    if (!tensorrt_utils::expectDims(dimsCondition, {-1, 3, imgH, imgW})) {
+      throw std::runtime_error(absl::StrFormat(
+          "Invalid shape for %s tensor of the tensorRT engine: %s (expected {-1, 3, %d, %d}).",
+          ioNameCondition_, tensorrt_utils::dimsToStr(dimsCondition), imgH, imgW));
     }
-    if (!tensorrt_utils::expectDims(maskDims, {-1, 1, imgH, imgW})) {
-      throw std::runtime_error(absl::StrFormat("Invalid shape for %s tensor: %s.",
-                                               FFSWP_TENSORNAME_MASK,
-                                               tensorrt_utils::dimsToStr(maskDims)));
+    if (!tensorrt_utils::expectDims(dimsMask, {-1, 1, imgH, imgW})) {
+      throw std::runtime_error(absl::StrFormat(
+          "Invalid shape for %s tensor of the tensorRT engine: %s (expected {-1, 1, %d, %d}).",
+          ioNameMask_, tensorrt_utils::dimsToStr(dimsMask), imgH, imgW));
     }
-    if (!tensorrt_utils::expectDims(inpaintedDims, {-1, 3, imgH, imgW})) {
-      throw std::runtime_error(absl::StrFormat("Invalid shape for %s tensor: %s.",
-                                               FFSWP_TENSORNAME_INPAINTED,
-                                               tensorrt_utils::dimsToStr(inpaintedDims)));
+    if (!tensorrt_utils::expectDims(dimsOutput, {-1, 3, imgH, imgW})) {
+      throw std::runtime_error(absl::StrFormat(
+          "Invalid shape for %s tensor of the tensorRT engine: %s (expected {-1, 3, %d, %d}).",
+          ioNameOutput_, tensorrt_utils::dimsToStr(dimsOutput), imgH, imgW));
     }
     // Set image dimensions
     imgH_ = imgH;
     imgW_ = imgW;
-    imgC_ = conditionDims.d[1];  // Assuming channels are at dimension 1
+    imgC_ = dimsCondition.d[1];  // Assuming channels are at dimension 1
   }
 
   // Swap faces in the source image
@@ -107,6 +107,9 @@ class FastFaceSwapper {
   std::int64_t imgH_;                                             // Image height
   std::int64_t imgW_;                                             // Image width
   std::int64_t imgC_;                                             // Number of image channels
+  std::string ioNameCondition_;
+  std::string ioNameMask_;
+  std::string ioNameOutput_;
 };
 
 }  // namespace ffswp
